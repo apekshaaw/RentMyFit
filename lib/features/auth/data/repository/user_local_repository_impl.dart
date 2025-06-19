8c8a4d4e1dd37510de1d6535bb0c0ac76efee038
apekshaaw <apeksha.wagle2017@gmail.com>
2025-06-20 01:34:55 +0545

    feat(auth-data): add UserRepositoryImpl

---
+++ lib/features/auth/data/repository/user_local_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:rent_my_fit/features/auth/data/data_source/user_local_datasource.dart';
+import 'package:rent_my_fit/features/auth/data/model/user_hive_model.dart';
+import 'package:rent_my_fit/features/auth/domain/entity/user_entity.dart';
+import 'package:rent_my_fit/features/auth/domain/repository/user_repository.dart';
+
+class UserLocalRepository implements UserRepository {
+  final UserLocalDatasource datasource;
+
+  UserLocalRepository(this.datasource);
+
+  @override
+  Future<void> registerUser(UserEntity user) async {
+    final hiveModel = UserHiveModel.fromEntity(user);
+    await datasource.saveUser(hiveModel);
+  }
+
+  @override
+  Future<UserEntity?> loginUser(String username) async {
+    final hiveModel = await datasource.getUser(username);
+    return hiveModel?.toEntity();
+  }
+}
