9606ab18435bbf229454b6bd13aab73bbd7164df
apekshaaw <apeksha.wagle2017@gmail.com>
2025-07-03 22:44:47 +0545

    Sprint-4: Setup service locator for dependency injection

--- lib/app/service_locator.dart
+++ lib/app/service_locator.dart
@@ -1,13 +1,19 @@
 import 'package:get_it/get_it.dart';
-import 'package:rent_my_fit/features/auth/data/data_source/user_local_datasource.dart';
-import 'package:rent_my_fit/features/auth/data/repository/user_local_repository_impl.dart';
-import '../../features/auth/domain/repository/user_repository.dart';
+import 'package:http/http.dart' as http;
+import 'package:rent_my_fit/features/auth/data/repository/user_remote_repository_impl.dart';
+import 'package:rent_my_fit/features/auth/domain/usecases/register_user.dart';
+import 'package:rent_my_fit/features/auth/domain/usecases/login_user.dart';
+import 'package:rent_my_fit/features/auth/domain/repository/user_repository.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  // Repository
+  sl.registerLazySingleton(() => http.Client());
+
   sl.registerLazySingleton<UserRepository>(
-    () => UserLocalRepository(UserLocalDatasource()),
+    () => UserRemoteRepositoryImpl(sl()),
   );
+
+  sl.registerLazySingleton(() => RegisterUser(sl()));
+  sl.registerLazySingleton(() => LoginUser(sl()));
 }
