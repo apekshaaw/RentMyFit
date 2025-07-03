dfb610d498534ec47b49312c63b4be77901612ec
apekshaaw <apeksha.wagle2017@gmail.com>
2025-07-03 22:45:56 +0545

    Sprint-4: Update UserEntity to support remote login/register

--- lib/features/auth/domain/entity/user_entity.dart
+++ lib/features/auth/domain/entity/user_entity.dart
@@ -1,9 +1,11 @@
 class UserEntity {
+  final String? id;
   final String name;
   final String email;
   final String password;
 
   UserEntity({
+    this.id,
     required this.name,
     required this.email,
     required this.password,
